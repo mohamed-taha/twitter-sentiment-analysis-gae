30beebaf7a30ef448694ef8c6458e9cecef9a629
Yudi Xue <binarycrayon@gmail.com>
2013-04-06 13:50:27 -0600

    -- add bootstrap

--- appengine/main.py
+++ appengine/main.py
@@ -12,5 +12,24 @@ CONFIG = {
     },
 }
 
+JINJA_DEBUG = True
+
+def enable_jinja2_debugging():
+    """Enables blacklisted modules that help Jinja2 debugging.
+        regarding the ImportError, read:
+        http://jinja.pocoo.org/docs/faq/#my-tracebacks-look-weird-what-s-happening
+    """
+    if not JINJA_DEBUG:
+        return
+    try:
+        from google.appengine.tools.dev_appserver import HardenedModulesHook
+        HardenedModulesHook._WHITE_LIST_C_MODULES += ['_ctypes', 'gestalt']
+    except ImportError:
+        pass # not available on production environment, but we may still want DEBUG on
+
+
+
+enable_jinja2_debugging()
+
 APP = WSGIApplication(ROUTES, debug=True, config=CONFIG)
 
